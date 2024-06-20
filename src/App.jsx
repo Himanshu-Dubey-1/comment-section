62acaf408e3081e9c7676c9b90ec3cdfd461b56e
Himanshu-Dubey-1 <Himanshudubey241@gmail.com>
2024-06-21 02:31:55 +0530

    new-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,72 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [values, setvalues] = useState([]);
-  const [comment, setComment] = useState("");
-  const [users, setusers] = useState([]);
-  const [name, setName] = useState("");
+  const [todos, settodos] = useState([]);
+  const [email, setemail] = useState("");
+  const [comment, setcomment] = useState("");
 
-  const commentText = (e) => {
-    setComment(e.target.value);
-  };
-
-  const addComment = () => {
-    if (name == "" || comment == "") {
-      alert("Please Enter The Correct Details");
-    } else {
-      setvalues([...values, comment]);
-      setusers([...users, name]);
-      setComment("");
-      setName("");
+  useEffect(() => {
+    let savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      settodos(JSON.parse(savedTodos));
     }
-  };
+  }, []);
 
-  const commentName = (e) => {
-    setName(e.target.value);
+  const onSubmit = () => {
+    if (email.trim() && comment.trim()) {
+      const newTodo = { email, comment };
+      settodos([...todos, newTodo]);
+      setemail("");
+      setcomment("");
+      localStorage.setItem("todos", JSON.stringify([...todos, newTodo]));
+    }
   };
 
   return (
     <>
-      <div className="container">
-        <div className="input-field">
+      <div className=" bg-yellow-100 min-h-screen">
+        <h1 className="text-center font-bold text-4xl border bg-slate-700 text-white py-3">Comment Box</h1>
+        <div className="fixed overflow-x-auto max-h-[57vh] mx-10">
+          {todos.map((todo, index) => (
+            <ol key={index}>
+              <li className="font-bold text-xl items-center ">
+                <lord-icon
+                  src="https://cdn.lordicon.com/kthelypq.json"
+                  trigger="hover"
+                  style={{"width":"35px", "height":"35px","paddingTop":"10px", "marginRight":"8px"}}
+
+                ></lord-icon>
+                {todo.email}
+              </li>
+              <li className="p-2 bg-slate-200 rounded-3xl mr-20 ml-10">{todo.comment}</li>
+            </ol>
+          ))}
+        </div>
+
+        <div className="flex-col justify-between text-center fixed w-[99.5%] bottom-0 mx-2">
           <input
-            className="email-box"
+            className="border border-blue-800 rounded-2xl h-12 w-[95%] m-1 px-2"
+            value={email}
+            placeholder="Enter your Email"
             type="email"
-            value={name}
-            placeholder="Enter your Email-Id"
-            onChange={commentName}
+            onChange={(e) => setemail(e.target.value)}
           />
-          <br />
           <textarea
-            className="comment-box"
-            type="text"
+            className="border border-blue-800 rounded-2xl h-36 w-[95%] text-wrap m-2 px-2"
             value={comment}
-            placeholder="Comment"
-            onChange={commentText}
+            placeholder="Enter Comment"
+            type="text"
+            onChange={(e) => setcomment(e.target.value)}
           ></textarea>
-          <br />
-          <button onClick={addComment} className="btn">
+          <button
+            className="bg-green-500 rounded-full
+          px-4 py-2 border border-green-900
+          mb-4 text-xl"
+            onClick={onSubmit}
+          >
             Submit Comment
           </button>
-        </div>
-        <div className="comments">
-          <h1>Comment Box</h1>
-          <hr />
-          <ul className="lists">
-            {values.map((value, index) => (
-              <li key={index}>
-                <h2 className="users-email">
-                  <b>{users[index]}</b>
-                </h2>
-                <span>{value}</span>
-              </li>
-            ))}
-          </ul>
         </div>
       </div>
     </>
